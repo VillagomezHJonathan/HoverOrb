7417bbc23cc0011f73ee830d730a50ac6a372584
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2021-06-08 02:17:51 -0400

    Added ball class and a basic animation that makes the ball levitate on touch

--- core/src/com/jugoapps/hoverorb/GameStage.java
+++ core/src/com/jugoapps/hoverorb/GameStage.java
@@ -2,32 +2,55 @@ package com.jugoapps.hoverorb;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.actions.MoveByAction;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 
 public class GameStage extends Stage {
     private boolean visible = true;
 
+    public static class BallActor extends Actor {
+        Sprite ball = new Sprite(new Texture("ball.png"));
+
+        public BallActor() {
+            setBounds(ball.getX(), ball.getY(), ball.getWidth(), ball.getHeight());
+            setTouchable(Touchable.enabled);
+
+            addListener(new ClickListener() {
+                @Override
+                public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                    MoveByAction mba = new MoveByAction();
+                    mba.setAmount(0f,100f);
+                    mba.setDuration(5f);
+
+                    BallActor.this.addAction(mba);
+
+                    return super.touchDown(event, x, y, pointer, button);
+                }
+            });
+        }
+
+        @Override
+        protected void positionChanged() {
+            ball.setPosition(getX(), getY());
+        }
+
+        @Override
+        public void draw(Batch batch, float parentAlpha) {
+            ball.draw(batch);
+        }
+    }
+
     public GameStage(Texture ballTexture,
                      final StageInterface stageInterface) {
 
-        int halfWidth = Gdx.graphics.getWidth() / 2;
-
-        final Image ball = new Image(ballTexture);
-        ball.setSize(250, 250);
-        ball.setX(halfWidth - (ball.getWidth() / 2));
-        ball.addListener(new ClickListener() {
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                setVisible(false);
-                stageInterface.goToHome();
-                return super.touchDown(event, x, y, pointer, button);
-            }
-        });
-
+        BallActor ball = new BallActor();
         addActor(ball);
     }
 
