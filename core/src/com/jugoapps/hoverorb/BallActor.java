874f3e04881ea18afc69074fc3fbba9eb4744e88
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2021-06-09 15:02:14 -0400

    Added ball gravity and on touch bounce

--- core/src/com/jugoapps/hoverorb/BallActor.java
+++ core/src/com/jugoapps/hoverorb/BallActor.java
@@ -3,7 +3,6 @@ package com.jugoapps.hoverorb;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
@@ -21,19 +20,12 @@ public class BallActor extends Image {
     }
 
     public BallActor(Texture ball) {
-
         super(ball);
 
         setSize(250, 250);
         ballStartPosX = screenCenterX / 2 - getWidth() / 2;
-        setPosition(ballStartPosX, 1000);
+        setX(ballStartPosX);
         setBounds(getX(), getY(), getWidth(), getHeight());
-        
-        MoveToAction gravity = new MoveToAction();
-        gravity.setPosition(getX(), 0f);
-        gravity.setDuration(0.50f);
-
-        BallActor.this.addAction(gravity);
 
     }
 
--- core/src/com/jugoapps/hoverorb/HoverOrbMain.java
+++ core/src/com/jugoapps/hoverorb/HoverOrbMain.java
@@ -2,39 +2,69 @@ package com.jugoapps.hoverorb;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class HoverOrbMain extends ApplicationAdapter implements StageInterface {
 
+	ScreenViewport screenViewport;
+	OrthographicCamera camera;
+
 	StartStage startStage;
+
 	GameStage gameStage;
+	Actor ball;
+
 	SettingsStage settingsStage;
+
 	ThemesStage themesStage;
 
-	Texture playBtn;
-	Texture settingsBtn;
-	Texture homeBtn;
-	Texture themesBtn;
-	Texture ball;
+	Texture playBtnTexture;
+	Texture settingsBtnTexture;
+	Texture homeBtnTexture;
+	Texture themesBtnTexture;
+	Texture ballTexture;
+
+	float ballY;
+	float gravity;
+	float velocityY;
+	float forceY;
 
 	@Override
 	public void create () {
 
-		playBtn = new Texture("play_button.png");
-		settingsBtn = new Texture("settings_button.png");
-		homeBtn = new Texture("home_button.png");
-		themesBtn = new Texture("themes_button.png");
-		ball = new Texture("ball.png");
+		screenViewport = new ScreenViewport();
+		camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
-		startStage = new StartStage(playBtn, settingsBtn, themesBtn, this);
-		gameStage = new GameStage(ball, this);
-		settingsStage = new SettingsStage(homeBtn, this);
-		themesStage = new ThemesStage(homeBtn, this);
+		playBtnTexture = new Texture("play_button.png");
+		settingsBtnTexture = new Texture("settings_button.png");
+		homeBtnTexture = new Texture("home_button.png");
+		themesBtnTexture = new Texture("themes_button.png");
+		ballTexture = new Texture("ball.png");
+
+		startStage = new StartStage(playBtnTexture, settingsBtnTexture,
+				themesBtnTexture, this);
+
+		gameStage = new GameStage(ballTexture, this);
+		ball = gameStage.getActors().get(0);
+
+		settingsStage = new SettingsStage(homeBtnTexture, this);
+
+		themesStage = new ThemesStage(homeBtnTexture, this);
 
 		/* Change this input processor to startStage and the booleans in
 		* GameStage.java and StartStage.java when done the gameStage */
 		Gdx.input.setInputProcessor(gameStage);
+
+		ballY = 0;
+		gravity = 5;
+		forceY = 100;
 	}
 
 	@Override
@@ -45,6 +75,20 @@ public class HoverOrbMain extends ApplicationAdapter implements StageInterface {
 		startStage.draw();
 
 		gameStage.draw();
+		velocityY += gravity;
+		ballY -= velocityY;
+		ball.setPosition(ball.getX(), ballY);
+		if (ball.getY() <= 0) {
+			ballY = 0;
+			velocityY = 0;
+		}
+		ball.addListener(new ClickListener() {
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+				velocityY = -forceY;
+				return super.touchDown(event, x, y, pointer, button);
+			}
+		});
 
 		settingsStage.draw();
 
@@ -54,24 +98,32 @@ public class HoverOrbMain extends ApplicationAdapter implements StageInterface {
 	@Override
 	public void goToGameSetup() {
 		gameStage.setVisible(true);
+		gameStage.setViewport(screenViewport);
+		gameStage.getBatch().setProjectionMatrix(camera.combined);
 		Gdx.input.setInputProcessor(gameStage);
 	}
 
 	@Override
 	public void goToHome() {
 		startStage.setVisible(true);
+		startStage.setViewport(screenViewport);
+		startStage.getBatch().setProjectionMatrix(camera.combined);
 		Gdx.input.setInputProcessor(startStage);
 	}
 
 	@Override
 	public void goToSettings() {
 		settingsStage.setVisible(true);
+		settingsStage.setViewport(screenViewport);
+		settingsStage.getBatch().setProjectionMatrix(camera.combined);
 		Gdx.input.setInputProcessor(settingsStage);
 	}
 
 	@Override
 	public void goToThemes() {
 		themesStage.setVisible(true);
+		themesStage.setViewport(screenViewport);
+		themesStage.getBatch().setProjectionMatrix(camera.combined);
 		Gdx.input.setInputProcessor(themesStage);
 	}
 
@@ -81,10 +133,10 @@ public class HoverOrbMain extends ApplicationAdapter implements StageInterface {
 		gameStage.dispose();
 		settingsStage.dispose();
 		themesStage.dispose();
-		playBtn.dispose();
-		settingsBtn.dispose();
-		themesBtn.dispose();
-		ball.dispose();
+		playBtnTexture.dispose();
+		settingsBtnTexture.dispose();
+		themesBtnTexture.dispose();
+		ballTexture.dispose();
 	}
 
 }
