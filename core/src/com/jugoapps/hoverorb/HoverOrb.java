4f220de3cf27753f1fb9d15f830d7a04db3851fe
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2022-05-31 22:42:06 -0400

    Added ball image

--- core/src/com/jugoapps/hoverorb/HoverOrb.java
+++ core/src/com/jugoapps/hoverorb/HoverOrb.java
@@ -1,31 +1,32 @@
 package com.jugoapps.hoverorb;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class HoverOrb extends ApplicationAdapter {
+public class HoverOrb extends Game {
 	SpriteBatch batch;
-	Texture img;
+	Texture ballTexture;
+
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		ballTexture = new Texture("ball.png");
+
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
 	}
 }
